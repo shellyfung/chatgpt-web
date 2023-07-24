e37902933968859af169f2e0932aa77a88f3c0d9
shellyfung <shellyfung2013@gmail.com>
2023-07-24 22:07:13 +0800

    pref:：修改启动代码

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
+git clone -b local https://github.com/shellyfung/chatgpt-web.git
 
 cd ./service
-nohup pnpm start > service.log &
+nohup pnpm start > service.log 2>&1 &
 echo "Start service complete!"
 
 
 cd ..
 echo "" > front.log
-nohup pnpm dev > front.log &
+nohup pnpm dev > front.log 2>&1 &
 echo "Start front complete!"
-tail -f front.log
+# tail -f front.log
