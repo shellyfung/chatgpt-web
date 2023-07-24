61dc9396c4ed1deb84f85b9bd70f31b319a74706
shellyfung <shellyfung2013@gmail.com>
2023-07-24 22:39:01 +0800

    fix：增加依赖安装

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 git pull
 
 cd ./service
+pnpm install
 nohup pnpm start > service.log 2>&1 &
 echo "Start service complete!"
 
 
 cd ..
+pnpm bootstrap
 echo "" > front.log
 nohup pnpm dev > front.log 2>&1 &
 echo "Start front complete!"
